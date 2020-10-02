8e7d585842f47e8d2aa57a21f1ff8429b8a53a6e
Kristy Keller <keller.kristy@outlook.com>
2020-10-02 14:22:41 -0400

    backend added and delete notes updated

--- server.js
+++ server.js
@@ -1,54 +1,85 @@
-
-const express = require('express');
-const db = require("./db/db.json");
+  
+var express = require("express");
 var path = require("path");
 var fs = require("fs");
+var notes = require("./db/db.json")
 
-const app = express();
-const PORT = process.env.PORT || 8080;
-
+var app = express();
+var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 currentID = notes.length;
 
-// Data
 // API Routes
-require("./routes")
-
-
-// GET /api/notes
-
-app.get("/", function (req, res) {
-    res.send("");
-});
 
 app.get("/api/notes", function (req, res) {
+
     return res.json(notes);
-}
-// get data somehow from db.json 
-// return res.json(data);
+});
 
-// POST /api/notes
-// Delete /api/notes/:id
-// /api/notes/1
+app.post("/api/notes", function (req, res) {
+    var newNote = req.body;
 
+    newNote["id"] = currentID +1;
+    currentID++;
+    console.log(newNote);
+
+    notes.push(newNote);
+
+    rewriteNotes();
+
+    return res.status(200).end();
+});
+
+app.delete("/api/notes/:id", function (req, res) {
+    res.send('Got a DELETE request at /api/notes/:id')
+
+    var id = req.params.id;
+
+    var idLess = notes.filter(function (less) {
+        return less.id < id;
+    });
+
+    var idGreater = notes.filter(function (greater) {
+        return greater.id > id;
+    });
+
+    notes = idLess.concat(idGreater);
+
+    rewriteNotes();
+})
+
+// Access files in "public" folder
+
+app.use(express.static("public"));
 
 // HTML Routes
 
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
 
-// GET /notes to get notes.html
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
 
-// fs.writeFile(destination, db, function(Err) { }) 
-
-// Save deleted version of notes with that note removed 
-
-// get * to get index.html
-
+// Listen
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
-// db.json to
 
+// Functions
+
+function rewriteNotes() {
+    fs.writeFile("db/db.json", JSON.stringify(notes), function (err) {
+        if (err) {
+            console.log("error")
+            return console.log(err);
+        }
+
+        console.log("Success!");
+    });
+}
